681ab0a1d703f6be785e2757bd4ce61e1f26b8b0
ewq-qwe <pavliuk.mykyta@chnu.edu.ua>
2026-06-04 17:21:22 +0300

    added color theme and font

--- desktopApp/src/main/kotlin/com/ua/pavliyk/advance/main.kt
+++ desktopApp/src/main/kotlin/com/ua/pavliyk/advance/main.kt
@@ -1,16 +1,19 @@
 package com.ua.pavliyk.advance
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.ua.pavliyk.advance.di.initKoin
 import com.ua.pavliyk.advance.presentation.App
+import com.ua.pavliyk.advance.ui.theme.AppTheme
+
 
 fun main() = application {
-    initKoin { printLogger() }
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Advance",
-    ) {
-        App()
-    }
+        initKoin { printLogger() }
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Advance",
+        ) {
+            App()
+        }
 }
--- shared/src/commonMain/kotlin/com/ua/pavliyk/advance/presentation/App.kt
+++ shared/src/commonMain/kotlin/com/ua/pavliyk/advance/presentation/App.kt
@@ -2,43 +2,31 @@ package com.ua.pavliyk.advance.presentation
 
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.ua.pavliyk.advance.ui.theme.AppTheme
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun App(
     viewModel: AppViewModel = koinViewModel(),
 ) {
-    MaterialTheme {
+    AppTheme {
 
         val state by viewModel.state.collectAsStateWithLifecycle()
 
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(MaterialTheme.colorScheme.background)
                 .systemBarsPadding()
                 .padding(16.dp)
                 .fillMaxSize(),
@@ -46,18 +34,10 @@ fun App(
         ) {
             AppContent(
                 state = state,
-                onGet = {
-                    viewModel.fetchPosts()
-                },
-                onPost = {
-                    viewModel.createPost()
-                },
-                onPut = {
-                    viewModel.updatePost()
-                },
-                onDelete = {
-                    viewModel.deletePost()
-                }
+                onGet = { viewModel.fetchPosts() },
+                onPost = { viewModel.createPost() },
+                onPut = { viewModel.updatePost() },
+                onDelete = { viewModel.deletePost() }
             )
         }
     }
@@ -136,7 +116,9 @@ private fun AppContent(
                 modifier = Modifier
                     .fillMaxSize()
                     .verticalScroll(scrollState),
-                text = it
+                text = it,
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.bodyLarge
             )
         }
 
